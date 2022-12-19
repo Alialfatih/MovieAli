86d73c16b904d9ff200c3e71dd7965cd0f295a94
Ali Al Fatih <yakusaali4@gmail.com>
2022-12-19 20:02:25 +0700

    Initial commit

---
+++ app/src/main/java/com/mayr/movieali/retrofit/Constant.java
@@ -0,0 +1,11 @@
+package com.mayr.movieali.retrofit;
+
+public class Constant {
+
+    public static String BASE_URL = "https://api.themoviedb.org/3/";
+    public static String LANGUAGE = "en-US";
+    public static String API_KEY = "8131d2d03603e170ce886ec2438dfa0c";
+    public static String BACKDROP_PATH = "https://image.tmdb.org/t/p/original/";
+    public static String POSTER_PATH = "https://image.tmdb.org/t/p/original/";
+
+}
